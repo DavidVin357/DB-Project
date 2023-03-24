5b2bcaea0990c4a072d002923eccc097a329bdb7
david <davydviniarskyi.dev@gmail.com>
2023-03-24 10:04:46 +0800

    fix sql schema

--- SQLSchema.sql
+++ SQLSchema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS users (
-    first_name VARCHAR(64) NOT NULL
+CREATE TABLE IF NOT EXISTS customers (
+    first_name VARCHAR(64) NOT NULL,
 	last_name VARCHAR(64) NOT NULL,
 	email VARCHAR(64) PRIMARY KEY
 );
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS drivers (
     first_name VARCHAR(64) NOT NULL,
 	last_name VARCHAR(64) NOT NULL,
 	email VARCHAR(64) PRIMARY KEY,
-	license_number CHAR(9) UNIQUE NOT NULL,
-	car_number CHAR(8) UNIQUE NOT NULL
+	license_number CHAR(9) UNIQUE NOT NULL CHECK (length(license_number) = 9),
+	car_number CHAR(8) UNIQUE NOT NULL CHECK (length(car_number) = 8)
 );
 
 CREATE TABLE IF NOT EXISTS rides (
-    customer_email references customers(email) NOT NULL,
-	driver_email VARCHAR(64) references drivers(email) NOT NULl,
+    customer_email VARCHAR(64) references customers(email) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+	driver_email VARCHAR(64) references drivers(email) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
 	start_location VARCHAR(64) NOT NULL,
 	end_location VARCHAR(64) NOT NULL,
 	departure_time TIME NOT NULL,
